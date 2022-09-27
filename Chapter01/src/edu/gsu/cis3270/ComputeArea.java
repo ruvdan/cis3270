35f1c499967fe620d890d3041af5cbc2cdab07ae
sandarbhkapur <sandarbhkapur@c02cd2cnlvdlmbp.wifi.gsu.edu>
2022-09-27 18:42:59 -0400

    pushing my compute area file

---
+++ Chapter01/src/edu/gsu/cis3270/ComputeArea.java
@@ -0,0 +1,34 @@
+package edu.gsu.cis3270;
+
+import java.util.Scanner;
+
+
+public class ComputeArea {
+	
+	public static void main(String[] args) {
+		
+		double radius = 20;
+		double area = 3.14 * radius * radius;
+		
+		final byte B;
+		
+		int C = 10;
+		
+		B = (byte)(C + 3);
+		
+		System.out.print("Value of b is "+ B);
+		
+		
+		System.out.println("The area of a circle with radius " + radius + " is " + area);
+	
+	
+	}
+
+}
+
+
+
+
+
+
+
