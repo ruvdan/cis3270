575d54ad253042bee12f941101eece6f755c3800
juanrosas <juanrosas@localhost>
2022-09-27 21:17:07 -0400

    first test commit

---
+++ TestProject/src/edu/gsu/cis3270/team/testing.java
@@ -0,0 +1,17 @@
+package edu.gsu.cis3270.team;
+
+public class testing {
+	
+	public static void main(String[] args) {
+		
+		
+		for (int i = 0; i < 5; i ++) {
+			
+			System.out.println("Let's Go!!!");
+			System.out.println("-----------");
+			
+		}
+				
+	}
+
+}
